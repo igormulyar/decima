00682e7b6ca2771ef953ad6d4ceb7d93cc2bdeec
imuliar <igormulyar@ukr.net>
2020-02-05 19:27:56 +0200

    minor changes

--- src/main/java/com/imuliar/decima/service/processors/CancelBookingProcessor.java
+++ src/main/java/com/imuliar/decima/service/processors/CancelBookingProcessor.java
@@ -1,11 +1,17 @@
 package com.imuliar.decima.service.processors;
 
+import com.imuliar.decima.entity.Booking;
+import com.imuliar.decima.entity.Slot;
+import com.imuliar.decima.service.impl.BookingRequestsSupplier;
 import com.imuliar.decima.service.util.InlineKeyboardMarkupBuilder;
 import java.time.LocalDate;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 
 import static com.imuliar.decima.service.util.Callbacks.CANCEL_MY_BOOKING;
@@ -21,7 +27,8 @@ import static com.imuliar.decima.service.util.Callbacks.TO_BEGINNING;
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CancelBookingProcessor extends AbstractUpdateProcessor {
 
-    private static final String RELEASE_MESSAGE_PATTERN = ":information_source: BOT NOTIFICATION:\n[Someone](tg://user?id=%d) has shared their slot for today.";
+    @Autowired
+    private BookingRequestsSupplier bookingRequestsSupplier;
 
     @Override
     public boolean isMatch(Update update) {
@@ -30,12 +37,25 @@ public class CancelBookingProcessor extends AbstractUpdateProcessor {
 
     @Override
     protected void doProcess(Update update, Long chatId) {
-        int userId = chatId.intValue();
-        if (getBookingRepository().findByUserIdAndDate(userId, LocalDate.now()).isPresent()) {
-            getBookingRepository().removeByUserIdAndDate(userId, LocalDate.now());
+        Integer userId = chatId.intValue();
+        LocalDate today = LocalDate.now();
+        Optional<Booking> bookingToDrop = getBookingRepository().findByUserIdAndDate(userId, today);
+
+        if (bookingToDrop.isPresent()) {
+            Slot slot = bookingToDrop.get().getSlot();
+            getBookingRepository().removeByUserIdAndDate(userId, today);
             getMessagePublisher().sendMessageWithKeyboard(chatId, getMsg("msg.drop_booking_confirmed"),
                     new InlineKeyboardMarkupBuilder()
                             .addButton(new InlineKeyboardButton(getMsg("btn.back")).setCallbackData(TO_BEGINNING)).build());
+
+            Optional<User> possibleRequester = bookingRequestsSupplier.pullOutRandomRequester();
+            if(possibleRequester.isPresent()){
+                User requester = possibleRequester.get();
+                Booking booking = new Booking(requester.getId(), slot, today);
+                getBookingRepository().save(booking);
+                String msgForRequester = getMessageSourceFacade().getMsg("msg.pleb_shared_for_you", requester.getLanguageCode(), userId.toString(), slot.getNumber());
+                getMessagePublisher().sendMsgWithBackBtn(requester.getId().longValue(), msgForRequester);
+            }
         }
     }
 }
